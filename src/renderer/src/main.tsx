2430605231380f406558c889c5f268677e041979
Artemiy Polev <artemiy600@gmail.com>
2026-06-09 14:18:56 +0100

    feat: swap font to JetBrains Mono; add GitHub Actions CI/release workflow

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import '@fontsource/inter/600.css'
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/500.css'
+import '@fontsource/jetbrains-mono/600.css'
 import './theme.css'
 
 createRoot(document.getElementById('root')!).render(
